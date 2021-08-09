94c0ad8a8ef107088e8f216f785258ebd2739c2f
Jibran Supple <>
2021-08-09 15:56:10 -0400

    building out model for Sugar, not sure at this point if comments should just be part of context, or should context also pull in comments that are specifically added at a time when user is adding reading

--- honey/models/sugar.js
+++ honey/models/sugar.js
@@ -8,5 +8,25 @@ const sugarSchema = new Schema({
         type: Number,
         min: 1,
         max: 25, // else the validator should say// else the validator should say please enter in mmol/L or offer up a conversion if reading is out of bounds
-    }
+        required: true,
+    },
+    units: {
+        type: String,
+        required: true,
+        enum: [
+            'mmol/L',
+            'mg/dL'
+        ],
+        default: 'mmol/L'
+    },
+    readingType: {
+        type: String,
+        required: false,
+        enum: [
+            'pre-meal',
+            'post-meal',
+            'fasting',
+        ]
+    },
+    
 })
