5e9c8bf441fb51f6fcfc9f97bd288da8c222b15a
Jibran Supple <>
2021-08-09 18:22:05 -0400

    creating form and routes to handle posting, so far created the validation service but now needs prompt to handle.  Creating Jira issue for this as well HON-18.  Too many issues are building a web. Haven't tried adding a record to the db yet.

--- honey/controllers/sugars.js
+++ honey/controllers/sugars.js
@@ -3,8 +3,19 @@ const Sugar = require('../models/sugar');
 module.exports = {
     index,
     new: newSugar,
+    create,
 }
 // else the validator should say please enter in mmol/L or offer up a conversion if reading is out of bounds
+
+async function create(req, res) {
+    let valSugarData = validateSugarData(req.body);
+    
+    console.log(valSugarData);
+    
+    // try{
+    //     let newRecord = await Sugar.create();
+    // }
+}
 
 function newSugar(req, res) {
     // let newRecord = await Sugar.
@@ -26,4 +37,23 @@ async function index(req, res) {
         console.log(err);
         res.redirect('/index');
     }
+}
+
+
+function validateSugarData(inputData) {
+    let dataToBeValidated = inputData;
+    
+    
+    dataToBeValidated.reading = parseFloat(inputData.reading);
+    if (dataToBeValidated.reading > 25) {
+        // TODO: [HON-18] this needs to be fixed, min max needs to be added
+        console.log('Hi from sugarValidationService in sugarCtrl: We have failed with your input being high need unit prompt or conversion');
+    } else {
+
+        validatedData = dataToBeValidated;
+    
+        return validatedData;
+        
+    }
+
 }
