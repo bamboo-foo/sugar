0348481364ca3dba569c53917c9a6dbfed78f21d
Jibran Supple <>
2021-08-10 17:20:06 -0400

    adding detailed view and show route and controller

--- honey/controllers/sugars.js
+++ honey/controllers/sugars.js
@@ -1,68 +1,72 @@
-const Sugar = require('../models/sugar');
+const Sugar = require("../models/sugar"),
+  mongoose = require("mongoose");
 
 module.exports = {
-    index,
-    new: newSugar,
-    create,
-}
+  index,
+  new: newSugar,
+  create,
+  show,
+};
 // else the validator should say please enter in mmol/L or offer up a conversion if reading is out of bounds
 
-async function create(req, res) {
-    
-    let valSugarData = validateSugarData(req.body);
-     
-    try{
-        const newRecord = await new Sugar(valSugarData);
-        newRecord.save( function(err) {
-            if (err) return res.redirect('/sugars');
-            console.log('Hi from create in sugarCtrl, success: ', newRecord);
-            return res.redirect('/sugars');
-        })
-    } catch(err) {
-        console.log('Hi from create in sugarCtrl, there was an error: ', err);
-    }
+async function show(req, res) {
+  try {
+    let sugar = await Sugar.findById(req.params.id);
 
+    res.render("sugars/show", { sugar });
+  } catch (err) {
+    console.log("Hi from show in sugarCtrl, we have an error: ", err);
+  }
+}
+
+async function create(req, res) {
+  let valSugarData = validateSugarData(req.body);
+
+  try {
+    const newRecord = await new Sugar(valSugarData);
+    newRecord.save(function (err) {
+      if (err) return res.redirect("/sugars");
+      console.log("Hi from create in sugarCtrl, success: ", newRecord);
+      return res.redirect("/sugars");
+    });
+  } catch (err) {
+    console.log("Hi from create in sugarCtrl, there was an error: ", err);
+  }
 }
 
 function newSugar(req, res) {
-    // let newRecord = await Sugar.
-    res.render('sugars/new', 
-        { 
-            title: 'Add a Sugar Reading',
-        }
-    );
+  // let newRecord = await Sugar.
+  res.render("sugars/new", {
+    title: "Add a Sugar Reading",
+  });
 }
 
 async function index(req, res) {
-    try{
-        let sugars = await Sugar.find({});
-        res.render('sugars/index', {
-            title: 'Sugar Records Analysis',
-            sugars,
-        });
-    } catch(err) {
-        console.log(err);
-        res.redirect('/index');
-    }
+  try {
+    let sugars = await Sugar.find({});
+    res.render("sugars/index", {
+      title: "Sugar Records Analysis",
+      sugars,
+    });
+  } catch (err) {
+    console.log(err);
+    res.redirect("/index");
+  }
 }
 
-
 function validateSugarData(inputData) {
-    let dataToBeValidated = inputData;
-    // TODO: [HON-25] conversion between units for storage
-    
-    
+  let dataToBeValidated = inputData;
+  // TODO: [HON-25] conversion between units for storage
 
-    dataToBeValidated.reading = parseFloat(inputData.reading);
-    if (dataToBeValidated.reading > 25) {
-        // TODO: [HON-18] this needs to be fixed, min max needs to be added
-        console.log('Hi from sugarValidationService in sugarCtrl: We have failed with your input being high need unit prompt or conversion');
-    } else {
+  dataToBeValidated.reading = parseFloat(inputData.reading);
+  if (dataToBeValidated.reading > 25) {
+    // TODO: [HON-18] this needs to be fixed, min max needs to be added
+    console.log(
+      "Hi from sugarValidationService in sugarCtrl: We have failed with your input being high need unit prompt or conversion"
+    );
+  } else {
+    validatedData = dataToBeValidated;
 
-        validatedData = dataToBeValidated;
-    
-        return validatedData;
-        
-    }
-
+    return validatedData;
+  }
 }
