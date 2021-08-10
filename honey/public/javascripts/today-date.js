e96712e670cf5b1585773ae9286bfaa1aa5af35f
BambooFoo <jsupple@ryerson.ca>
2021-08-10 12:45:36 -0400

    adding date to db and by js will auto fill todays date, but will need validation to double check in case client side js is disabled

---
+++ honey/public/javascripts/today-date.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", insertDate);
+
+const dateField = document.getElementById("date");
+
+function insertDate() {
+  // controller will validate even if client has js blocked
+  dateField.value = todaysDate();
+}
+
+function todaysDate() {
+  const dateEntered = new Date(),
+    year = dateEntered.getFullYear().toString(),
+    month = (dateEntered.getMonth() + 1).toString(),
+    date = dateEntered.getDate();
+
+  function unshiftZero(input) {
+    if (input < 10) {
+      let dateStr = input.toString();
+
+      return "0" + dateStr;
+    } else {
+      return input.toString();
+    }
+  }
+
+  let monthStr = unshiftZero(month),
+    dateStr = unshiftZero(date);
+
+  return year + "-" + monthStr + "-" + dateStr;
+}
